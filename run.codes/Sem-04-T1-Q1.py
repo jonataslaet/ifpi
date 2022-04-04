a477be7ad619539edcb77892bf579715dbd7d49f
jonataslaetifpi <catce.2022111mtds0347@aluno.ifpi.edu.br>
2022-04-04 11:27:41 -0300

    20220328
    Adição das questões da Tarefa 1 da Semana 04 ao run.codes
    Correções na solução da quinta questão da tarefa 1 da semana 4

---
+++ run.codes/Sem-04-T1-Q1.py
@@ -0,0 +1,8 @@
+def showFormatDate(d, m, a):
+    print(f'{d}/{m}/{a}')
+
+if __name__ == "__main__":
+    day = int(input())
+    month = int(input())
+    year = int(input())
+    showFormatDate(day, month, year)
---
+++ run.codes/Sem-04-T1-Q3.py
@@ -0,0 +1,7 @@
+def elevadoA3(n):
+    resultado = n ** 3
+    return resultado
+
+if __name__ == "__main__":
+    numero = int(input())
+    print(elevadoA3(numero))
